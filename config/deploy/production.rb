7fbf5e43ed43e7a63ad9a561fd77fd9915bb5d26
Kirill Shevchenko <kirills167@gmail.com>
2017-04-18 15:03:03 +0300

    continuous deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-role :app, %w{deployer@ip}
-role :web, %w{deployer@ip}
-role :db,  %w{deployer@ip}
+role :app, %w{deployer@107.170.15.159}
+role :web, %w{deployer@107.170.15.159}
+role :db,  %w{deployer@107.170.15.159}
 
 set :branch, fetch(:branch, 'master')
 set :deploy_to, '/home/deployer/www/project'
